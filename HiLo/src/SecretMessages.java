c3e36da042de31b204facf05687f47f8508b8255
shikjfly <shikjfly@gmail.com>
2018-08-05 09:46:28 +0800

    数字加密

--- HiLo/src/SecretMessages.java
+++ HiLo/src/SecretMessages.java
@@ -13,20 +13,32 @@ public class SecretMessages {
 		int keyVal = Integer.parseInt(scan.nextLine());
 //		char key = 13;
 		char key = (char) keyVal;
+		
 		for (int x = 0; x < message.length(); x++) {
 			char input = message.charAt(x);
+//			大写加密
 			if (input >= 'A' && input <='Z') {
 				input += key;
 				if (input > 'Z') 
 					input -= 26;
 				if (input < 'A')
 					input += 26;				
-			} else if (input >= 'a' && input <='z') {
+			} 
+//			小写加密
+			else if (input >= 'a' && input <='z') {
 				input += key;
 				if (input > 'z') 
 					input -= 26;
 				if (input < 'a')
 					input += 26;				
+			}
+//			数字加密
+			else if (input >= '0' && input <= '9') {
+				input += (keyVal%10);
+				if (input > '9') 
+					input -= 10;
+				if (input < '0')
+					input += 10;	
 			}
 			output += input;
 		}
