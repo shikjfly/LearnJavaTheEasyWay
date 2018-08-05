b3e41733b0206d709bf6404c976f358dc05a63a7
shikjfly <shikjfly@gmail.com>
2018-08-05 14:04:27 +0800

    画泡泡

--- HiLo/src/BubbleDraw.java
+++ HiLo/src/BubbleDraw.java
@@ -1,10 +1,15 @@
 import javax.swing.JFrame;
 
 public class BubbleDraw extends JFrame {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("Xzhao's BubbleDraw App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new BubblePanel());
+		frame.setSize(new java.awt.Dimension(600, 400));
+		frame.setVisible(true);
+		
 	}
 
 }
--- HiLo/src/BubblePanel.java
+++ HiLo/src/BubblePanel.java
@@ -1,10 +1,48 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JPanel;
+import org.omg.CORBA.PRIVATE_MEMBER;
 
-public class BubblePanel extends JFrame {
+public class BubblePanel extends JPanel {
 
+	Random rand = new Random();
+	ArrayList<Bubble> bubbleList;
+	int size = 25;
+	
+	public  BubblePanel() {
+		bubbleList = new ArrayList<Bubble>();
+		setBackground(Color.BLACK);
+	}
+	
+	private class Bubble{
+		private int x;
+		private int y;
+		private int size;
+		private Color color;
+		public Bubble(int newX, int newY, int newSize) {
+			x = newX;
+			y = newY;
+			size = newSize;
+			color = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+		}
+		public void draw(Graphics canvas) {
+			canvas.setColor(color);
+			canvas.fillOval(x-size/2, y-size/2, size, size);
+		}
+	}
+//	Ìí¼ÓÆÁÄ»»æÍ¼·½·¨
+    public void paintComponent(Graphics canvas){
+        super.paintComponent(canvas);
+        for(Bubble b : bubbleList){
+            b.draw(canvas);
+        }
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
